21f3822d0dfaf5e207b43bddaa950b4cdd553658
GGeer3 <61589498+GGeer3@users.noreply.github.com>
2020-03-03 09:56:56 -0500

    Version 0.2

--- Colored Item Prices.user.js
+++ Colored Item Prices.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name Colored Item Prices
-// @version 0.1
+// @version 0.2
 // @description Calculates price differences of plushie and flower sets based on code input and outputs the differences and changes the color
 // @author Tazman777
 // @namespace Tazman777.Torn
@@ -44,7 +44,8 @@ function itemsLoaded(items) {
 
     var minprices = document.querySelectorAll('.minprice');
     var searchnames = document.querySelectorAll('.searchname');
-    console.log(minprices);
+//    console.log(minprices);
+
     for(var searchname of searchnames){
         searchname.style.display = 'none';
     }
@@ -60,25 +61,50 @@ function itemsLoaded(items) {
         }
     }
 
-    $.each(mySets, function(i, mySet) {
-        var sum = 0;
-//       console.log(items);
+    //Show results on page (attempt to find container, if it's not there we create it)
+    var container = $('#setCalculator');
+    if (!container.length) {
+        container = $('<div>').attr('id', 'setCalculator').addClass('msg right-round');
 
-        $.each(items, function(j, item) {
-            for (let i of mySets) {
-                if (i[0] === item.itemID) {
-                    if (parseInt(item.price) < parseInt(i[1])) {
-                        minprices[items.indexOf(item)].style.backgroundColor="#64d464";
-                        minprices[items.indexOf(item)].innerText = parseInt(item.price) - parseInt(i[1]);
-                    }
-                    else {
-                        minprices[items.indexOf(item)].style.backgroundColor="#ff5310bf";
-                        minprices[items.indexOf(item)].innerText = parseInt(item.price) - parseInt(i[1]);
-                    }
-                }
-            }
+        var wrapper = $('<div>').addClass('info-msg border-round').html($('<i>').addClass('info-icon'));
+        wrapper.append(container);
+        wrapper.prependTo($('.main-market-page'));
+    }
+
+    // Clear text
+    container.empty();
+
+    // Loop over itemsets and create a result message
+    var setSum = 0;
+	var itemSum = 0;
+
+    $.each(items, function(i, item) {
+        $.each(mySets, function(j, mySet) {
+            if (mySet[0] === item.itemID) {
+				setSum += parseInt(mySet[1]);
+				itemSum += parseInt(item.price);
+                 if (parseInt(item.price) < parseInt(mySet[1])) {
+                     minprices[items.indexOf(item)].style.backgroundColor="#64d464";
+                     minprices[items.indexOf(item)].innerText = (parseInt(item.price) - parseInt(mySet[1]))/10;
+                 }
+                 else {
+                     minprices[items.indexOf(item)].style.backgroundColor="#ff5310bf";
+                     minprices[items.indexOf(item)].innerText = (parseInt(item.price) - parseInt(mySet[1]))/10;
+                 }
+             }
         });
     });
+
+	// Show message on page
+    if (setSum > 0) {
+        // Generate final message
+        var message = ("One set costs " + accounting.formatMoney((itemSum/10), "$", 0) + " per point and was bought at " + accounting.formatMoney((setSum/10), "$", 0) + " per point. All price differences below are per point.");
+        // Append the message to the container
+        container.append($('<span>').html(message));
+    } else {
+        // No sets were present on this page
+        container.append($('<span>').html('No sets available.'));
+    }
 };
 
 $(document).ajaxComplete(function(e,xhr,settings){
